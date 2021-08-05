e77861c42a642b48a38ac86fb58a6602964f6de5
FedeHC <fede.hc@mail.com>
2021-08-05 20:00:55 -0300

    Completando componente de login, usando formik en validacion y envio de datos.

--- src/subcomponents/LoginForm.js
+++ src/subcomponents/LoginForm.js
@@ -1,67 +1,96 @@
-import { useState } from "react";
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+// React-Bootstrap:
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+// Formik/Yup:
+import { Formik } from "formik";
+import * as yup from "yup";
 
 
-function LoginForm() {
-  // useState:
-  const [validated, setValidated] = useState(false);
+function LoginForm({ getTokenHandler }) {
 
-  // Handler:
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    setValidated(true);
-  };
+  // Esquema para Yup con restricciones:
+  const schema = yup.object().shape({
+    email: yup.string().min(4, "Debe tener 4 o más caracteres de largo.")
+                       .max(40, "Debe tener no más de 50 caracteres de largo.")
+                       .required("Requerido."),
+    password: yup.string().min(4, "Debe tener 4 o más caracteres de largo.")
+                          .max(40, "Debe tener no más de 50 caracteres de largo.")
+                          .required("Requerido."),
+  });
 
+  // JSX:
   return (
     <Container fluid>
       <Row>
         <Col></Col>
 
+        {/* Columna con contenido */}
         <Col xs={8} md={6} lg={4}>
           <div id="loginDiv">
             <h1 className="text-center">[ Login ]</h1>
             <br />
 
-            <Form validated={validated}
-                  onSubmit={handleSubmit}
-                  noValidate>
+            {/* Compoinente Formik, necesario para formulario: */}
+            <Formik validationSchema={schema}
+                    onSubmit={getTokenHandler}
+                    initialValues={{ email: "", password: "" }}
+            >
+              {({ handleSubmit, handleChange, values, touched, isValid, errors }) => (
+                /* Formulario */
+                <Form onSubmit={handleSubmit} noValidate >
 
-              <Form.Group className="mb-3" controlId="formLoginEmail">
-                <Form.Label>Email:</Form.Label>
-                <Form.Control type="email"
-                              placeholder="Ingrese su email acá"
-                              size="lg" />
-              </Form.Group>
+                  {/* Email */}
+                  <Form.Group className="mb-3" controlId="formLoginEmail">
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control type="email"
+                                  name="email"
+                                  placeholder="Ingrese su email acá"
+                                  size="lg"
+                                  onChange={handleChange}
+                                  value={values.email}
+                                  isInvalid={!!errors.email} />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.email}
+                    </Form.Control.Feedback>
+                  </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formLoginPassword">
-                <Form.Label>Contraseña:</Form.Label>
-                <Form.Control type="password"
-                              placeholder="Ingrese su contraseña"
-                              size="lg" />
-              </Form.Group>
+                  {/* Contraseña */}
+                  <Form.Group className="mb-3" controlId="formLoginPassword">
+                    <Form.Label>Contraseña:</Form.Label>
+                    <Form.Control type="password"
+                                  name="password"
+                                  placeholder="Ingrese su contraseña"
+                                  size="lg"
+                                  onChange={handleChange}
+                                  value={values.password}
+                                  isInvalid={!!errors.password} />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.password}
+                    </Form.Control.Feedback>
+                  </Form.Group>
 
-              <br />
-              <div className="text-left">
-                <Button variant="outline-primary"
-                        type="submit"
-                        size="lg"
-                        id="buttonLogin"
-                >Enviar</Button>
-              </div>
+                  <br />
+                  
+                  {/* Boton Enviar */}
+                  <div className="text-left">
+                    <Button variant="outline-primary"
+                            type="submit"
+                            size="lg"
+                            id="buttonLogin"
+                    >Enviar</Button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
 
-            </Form>
             <br />
-          </div>          
+          </div>
         </Col>
-        
+
         <Col></Col>
       </Row>
     </Container>
