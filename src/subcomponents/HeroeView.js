99a74f92365ab475e8b6712c9fd939c95a0f0bf7
FedeHC <fede.hc@mail.com>
2021-08-06 17:52:11 -0300

    Mejorando columnas para ser responsive + agregando detalles basicos.

--- src/subcomponents/HeroeView.js
+++ src/subcomponents/HeroeView.js
@@ -1,25 +1,26 @@
 // React-Bootstrap:
-import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 
 
 function HeroeView() {
   return (
-    <Container fluid>
-      <Row>
-        <Col></Col>
-        
-        {/* Columna con contenido */}
-        <Col xs={8} md={6} lg={4}>
-          <div class="heroeView">
-            <h1>Heroe</h1>              
-          </div>
-        </Col>
-        
-        <Col></Col>
-      </Row>
-    </Container>
+    <Col md={{ span: 12}} lg={{ span: 6}} xl={{ span: 4}}>
+      <div class="heroeView">
+        <h1>Heroe</h1>
+        <img src="#"></img>
+        <p>Powerstats:</p>
+        <ol>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ol>
+        <p>Detalles</p>
+        <p>Borrar del equipo</p>
+      </div>
+    </Col>
   );
 }
 
