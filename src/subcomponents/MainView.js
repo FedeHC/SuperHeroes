ba0605645378b75bca1b866d584d1d266fd630f5
FedeHC <fede.hc@mail.com>
2021-08-05 21:49:59 -0300

    Agregando subcomponente MainView, como vista principal al loguearse con exito a la app.

---
+++ src/subcomponents/MainView.js
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
+function MainPage() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col></Col>
+        
+        {/* Columna con contenido */}
+        <Col xs={8} md={6} lg={4}>
+          <div id="welcomeDiv">
+            <h1>Bienvenido!</h1>              
+          </div>
+        </Col>
+        
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default MainPage;
