e718f59cad485ec120f5d8d5986e209caeaed41a
FedeHC <fede.hc@mail.com>
2021-08-06 17:48:38 -0300

    Mejorando formato de columnas y agregando subcomponentes HeroeView.

--- src/subcomponents/MainView.js
+++ src/subcomponents/MainView.js
@@ -1,22 +1,28 @@
+// React-Bootstrap:
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
+// Subcomponentes:
+import HeroeView from './HeroeView';
 
+
+// JSX:
 function MainView() {
   return (
     <Container fluid>
-      <Row>
-        <Col></Col>
-        
-        {/* Columna con contenido */}
-        <Col xs={8} md={6} lg={4}>
-          <div id="welcomeDiv">
-            <h1>Bienvenido!</h1>              
-          </div>
+      <Row>      
+        <Col xs={{ span: 10, offset: 1}}>
+          <Row id="welcomeDiv">
+            <h1 id="mainTitle">[El equipo]</h1>
+            <HeroeView />                       {/* Heroe 1 */}
+            <HeroeView />                       {/* Heroe 2 */}
+            <HeroeView />                       {/* Heroe 3 */}
+            <HeroeView />                       {/* Heroe 4 */}
+            <HeroeView />                       {/* Heroe 5 */}
+            <HeroeView />                       {/* Heroe 6 */}
+          </Row>
         </Col>
-        
-        <Col></Col>
       </Row>
     </Container>
   );
