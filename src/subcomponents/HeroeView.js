2f349f824a2636784f0dd2e2c5b787641e71bffd
FedeHC <fede.hc@mail.com>
2021-08-07 20:32:13 -0300

    Diferenciando la muestra de datos del subcomponente segun valor del prop recibido.

--- src/subcomponents/HeroeView.js
+++ src/subcomponents/HeroeView.js
@@ -1,24 +1,44 @@
 // React-Bootstrap:
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
-
-function HeroeView() {
+function HeroeView({ id }) {
   return (
-    <Col md={{ span: 12}} lg={{ span: 6}} xl={{ span: 4}}>
-      <div class="heroeView">
-        <h1>Heroe</h1>
-        <img src="#"></img>
-        <p>Powerstats:</p>
-        <ol>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ol>
-        <p>Detalles</p>
-        <p>Borrar del equipo</p>
+    <Col md={12} lg={6} xl={4}>
+      <div className="heroeView">
+
+        {/* CON HEROE */}
+        {id !== 0 &&
+          <>
+            <h1>Héroe</h1>
+            <img src="#" className="heroeImage" alt=""></img>
+            <p>Powerstats:</p>
+            <ol>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ol>
+            <div className="d-grid gap-2">
+              <Button variant="outline-primary" size="lg">Detalles</Button>
+              <Button variant="outline-danger" size="lg">Quitar Héroe</Button>
+            </div>
+          </>
+        }
+        
+        {/* SIN HEROE*/}
+        {id === 0 &&
+          <>
+            <h1>[Vacío]</h1>
+            <div className="emptyImage"></div>
+            <br />
+            <div className="d-grid gap-2">
+              <Button variant="outline-success" size="lg">Agregar Héroe</Button>
+            </div>
+          </>
+        }
       </div>
     </Col>
   );
