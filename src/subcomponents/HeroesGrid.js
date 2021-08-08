2f89b5889918a8e451ba91902ccb90e1ead911fb
FedeHC <fede.hc@mail.com>
2021-08-08 18:42:53 -0300

    Agregando un subcomponente SearchHero  antes de la tabla.

--- src/subcomponents/HeroesGrid.js
+++ src/subcomponents/HeroesGrid.js
@@ -4,17 +4,31 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Table from 'react-bootstrap/Table';
 
-// JSX:
-function HeroesGrid({ position }) {
+// Subcomponentes:
+import SearchHero from "./SearchHero";
+
+
+function HeroesGrid({ getMainViewHandler }) {
+  // JSX:
   return (
     <Container fluid>
       <Row>      
-        <Col xs={12}>
+        <Col xs={12} md={{span: 10, offset: 1}}>
           <Row id="heroesGridView">
-            <h1 id="heroesGridTitle">[Agregar SuperHeroe]</h1>
-            <Table>
+            <SearchHero getMainViewHandler={getMainViewHandler} />
 
+            {/*
+            <Table striped noborder hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Foto</th>
+                  <th>Nombre</th>
+                  <th>Opciones</th>
+                </tr>
+              </thead>
             </Table>
+            */}
           </Row>
         </Col>
       </Row>
