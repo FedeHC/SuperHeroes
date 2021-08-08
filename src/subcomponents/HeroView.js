0e390d7735e1f11a1337e88a9f34e352d5b4e3d0
FedeHC <fede.hc@mail.com>
2021-08-08 15:40:36 -0300

    Renombrado componente HeroeView a HeroViews.

---
+++ src/subcomponents/HeroView.js
@@ -0,0 +1,54 @@
+// React-Bootstrap:
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+
+function HeroView({ hero }) {
+  return (
+    <Col md={12} lg={6} xl={4}>
+      <div className="heroView">
+
+        {/* CON HEROE */}
+        {hero !== 0 &&
+          <>
+            <h1 className="heroName">Héroe</h1>
+            <img src="#" className="heroImage" alt=""></img>
+            <p>Powerstats:</p>
+            <ol>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ol>
+            <div className="d-grid gap-2">
+              <Button variant="outline-primary"
+                      size="lg"
+                      onClick="">Detalles</Button>
+              <Button variant="outline-danger"
+                      size="lg"
+                      onClick="">Quitar Héroe</Button>
+            </div>
+          </>
+        }
+        
+        {/* SIN HEROE*/}
+        {hero === 0 &&
+          <>
+            <h1>[Vacío]</h1>
+            <div className="emptyImage"></div>
+            <br />
+            <div className="d-grid gap-2">
+              <Button variant="outline-success"
+                      size="lg"
+                      onClick="">Agregar</Button>
+            </div>
+          </>
+        }
+      </div>
+    </Col>
+  );
+}
+
+export default HeroView;
