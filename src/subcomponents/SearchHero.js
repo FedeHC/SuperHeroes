a3d288d03e001ba1a21829002609d152343b9dfb
FedeHC <fede.hc@mail.com>
2021-08-08 23:15:46 -0300

    Modificando condicion en schema, moviendo de lugar handler y corrigiendo ancho de columnas.

--- src/subcomponents/SearchHero.js
+++ src/subcomponents/SearchHero.js
@@ -9,72 +9,65 @@ import { Formik } from "formik";
 import * as yup from "yup";
 
 
-function SearchHero({ getMainViewHandler }) {
+function SearchHero({ getMainViewHandler, searchHeroHandler }) {
   // Esquema para Yup (con restricciones):
   const schema = yup.object().shape({
-    search: yup.string().min(4, "Debe tener 4 o más caracteres de largo.")
-                        .max(40, "Debe tener no más de 40 caracteres de largo.")
+    search: yup.string().min(2, "Debe tener al menos 2 o más caracteres.")
+                        .max(40, "Debe tener no más de 40 caracteres.")
                         .required("Requerido."),
   });
-
-  // Handlers:
-  const getHeroHandler = () => {
-    console.log("- ENVIANDO PEDIDO GET A SUPERHEROES API.")
-  };
 
   // JSX:
   return (
     <Row>      
-      <Col xs={12} md={{span: 10, offset: 1}}>
-        <div id="searchDiv">
-          <h1 className="text-center">[Agregar Héroe]</h1>
-          <br />
+      <Col xs={{ span: 10, offset: 1 }} md={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
+        <h1 className="text-center">[Agregar un Héroe]</h1>
+        <br />
 
-          {/* Componente Formik, necesario para formulario: */}
-          <Formik validationSchema={schema}
-                  onSubmit={getHeroHandler}
-                  initialValues={{ search: "" }}
-          >
-            {({ handleSubmit, handleChange, values, errors }) => (
-              /* Formulario */
-              <Form onSubmit={handleSubmit} noValidate>
+        {/* Componente Formik, necesario para formulario: */}
+        <Formik validationSchema={schema}
+                onSubmit={searchHeroHandler}
+                initialValues={{ search: "" }}
+        >
+          {({ handleSubmit, handleChange, values, errors }) => (
+            /* Formulario */
+            <Form onSubmit={handleSubmit} noValidate>
 
-                {/* Input Buscar */}
-                <Form.Group className="mb-3" controlId="formLoginEmail">
-                  <Form.Label>Buscar:</Form.Label>
-                  <Form.Control type="text"
-                                name="search"
-                                placeholder="Ingrese un nombre a buscar (ej.: Batman)"
-                                size="lg"
-                                onChange={handleChange}
-                                value={values.search}                                  
-                                isInvalid={!!errors.search}
-                                autoFocus />
-                  <Form.Control.Feedback type="invalid">
-                    {errors.search}
-                  </Form.Control.Feedback>
-                </Form.Group>
-                <br />
-                
-                {/* Botón Buscar */}
-                  <Button variant="outline-primary"
-                          type="submit"
-                          size="lg"
-                  >Buscar Héroe</Button>
+              {/* Input Buscar */}
+              <Form.Group className="mb-3" controlId="formLoginEmail">
+                <Form.Label>Buscar:</Form.Label>
+                <Form.Control type="text"
+                              name="search"
+                              placeholder="Ingrese un nombre a buscar (ej: Batman)"
+                              size="lg"
+                              onChange={handleChange}
+                              value={values.search}                                  
+                              isInvalid={!!errors.search}
+                              autoFocus />
+                <Form.Control.Feedback type="invalid">
+                  {errors.search}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <br />
+              
+              {/* Botón Buscar */}
+                <Button variant="outline-primary"
+                        type="submit"
+                        size="lg"
+                >Buscar Héroe</Button>
 
-                  <Button variant="outline-secondary"
-                          type="input"
-                          size="lg"
-                          onClick={getMainViewHandler}
-                  >Volver a vista principal</Button>
+                <Button variant="outline-secondary"
+                        type="input"
+                        size="lg"
+                        onClick={getMainViewHandler}
+                >Volver a principal</Button>
 
-              </Form>
-            )}
-          </Formik>
+            </Form>
+          )}
+        </Formik>
 
-          <br />
-        </div>
-      /</Col>
+        <br />
+      </Col>
     </Row>
   );
 }
