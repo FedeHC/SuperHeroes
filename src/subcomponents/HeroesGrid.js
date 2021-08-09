779e1668c620d5039f68eb27eb1d085ca3cd35b0
FedeHC <fede.hc@mail.com>
2021-08-08 23:17:10 -0300

    Agregando handler a subcomponente SearchHero y presentando tabla basica para mostrar resultados de busqueda.

--- src/subcomponents/HeroesGrid.js
+++ src/subcomponents/HeroesGrid.js
@@ -1,34 +1,46 @@
 // React-Bootstrap:
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Table from 'react-bootstrap/Table';
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Table from "react-bootstrap/Table";
 
 // Subcomponentes:
 import SearchHero from "./SearchHero";
 
 
-function HeroesGrid({ getMainViewHandler }) {
+function HeroesGrid({ getMainViewHandler, searchHeroHandler, searchResults }) {
   // JSX:
   return (
     <Container fluid>
       <Row>      
         <Col xs={12} md={{span: 10, offset: 1}}>
           <Row id="heroesGridView">
-            <SearchHero getMainViewHandler={getMainViewHandler} />
-
-            {/*
-            <Table striped noborder hover>
+            <SearchHero getMainViewHandler={getMainViewHandler}
+                        searchHeroHandler={searchHeroHandler} />
+            <br />
+            
+            <Table striped hover>
               <thead>
                 <tr>
                   <th>#</th>
                   <th>Foto</th>
-                  <th>Nombre</th>
+                  <th>Apodo (Nombre real)</th>
                   <th>Opciones</th>
                 </tr>
               </thead>
+
+              <tbody>
+                {searchResults.map( (hero, index) => (
+                  <tr key={index}>
+                    <td>{index}</td>
+                    <td>{hero.image.url}</td>
+                    <td>{hero.name} {hero.biography["full-name"] ? "("+hero.biography["full-name"]+")" : ""}</td>
+                    <td>Agregar al equipo</td>
+                  </tr>   
+                ))}
+              </tbody>
             </Table>
-            */}
+
           </Row>
         </Col>
       </Row>
