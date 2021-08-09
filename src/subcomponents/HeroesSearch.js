5a5ec737b070aa588cee962f846f19adaa996606
FedeHC <fede.hc@mail.com>
2021-08-09 20:31:22 -0300

    Removiendo row innecesario.

--- src/subcomponents/HeroesSearch.js
+++ src/subcomponents/HeroesSearch.js
@@ -16,53 +16,52 @@ function HeroesSearch({ getMainViewHandler,
   // JSX:
   return (
     <Container fluid>
-      <Row>      
+      <Row id="heroesGridView">      
         <Col xs={12} md={{span: 10, offset: 1}}>
-          <Row id="heroesGridView">
-            <SearchForm getMainViewHandler={getMainViewHandler}
-                        searchHeroHandler={searchHeroHandler} />
-            <br />
+          <SearchForm getMainViewHandler={getMainViewHandler}
+                      searchHeroHandler={searchHeroHandler} />
+          <br />
 
-            {/* Tabla (si hubo resultados de búsqueda) */}
-            {searchResults && searchResults.response === "success" &&
-              <Table striped hover>
-                <thead>
-                  <tr>
-                    <th className="tableResults">#</th>
-                    <th className="tableResults">Foto</th>
-                    <th className="tableResults">Apodo (Nombre real)</th>
-                    <th className="tableResults">Opciones</th>
-                  </tr>
-                </thead>
+          {/* Tabla (si hubo resultados de búsqueda) */}
+          {searchResults && searchResults.response === "success" &&
+            <Table striped hover>
+              <thead>
+                <tr>
+                  <th className="tableResults">#</th>
+                  <th className="tableResults">Foto</th>
+                  <th className="tableResults">Apodo (Nombre real)</th>
+                  <th className="tableResults">Opciones</th>
+                </tr>
+              </thead>
 
-                <tbody>
-                  {searchResults.results.map( (hero, index) => (
-                    <tr key={index}>
-                      <td className="tableResults"><b>{index+1}</b></td>
-                      
-                      <td><img src={hero.image.url} className="imageHero" alt="" /></td>
-                      
-                      <td className="tableResults">{hero.name} {hero.biography["full-name"] ? "("+hero.biography["full-name"]+")" : ""}</td>
-                      
-                      <td><Button variant="outline-primary"
-                                  type="input"
-                                  size="lg"
-                                  onClick={ () => addHeroHandler(index)}
-                          >Agregar al equipo</Button></td>
-                    </tr>   
-                  ))}
-                </tbody>
-              </Table>
-            }
+              <tbody>
+                {searchResults.results.map( (hero, index) => (
+                  <tr key={index}>
+                    <td className="tableResults"><b>{index+1}</b></td>
+                    
+                    <td><img src={hero.image.url} className="imageHero" alt="" /></td>
+                    
+                    <td className="tableResults">{hero.name} {hero.biography["full-name"] ? "("+hero.biography["full-name"]+")" : ""}</td>
+                    
+                    <td><Button variant="outline-primary"
+                                type="input"
+                                size="lg"
+                                onClick={ () => addHeroHandler(index)}
+                        >Agregar al equipo</Button></td>
+                  </tr>   
+                ))}
+              </tbody>
+            </Table>
+          }
 
-            {/* Mensaje si no hubo resultados */}
-            {searchResults && searchResults.response === "error" &&
-            <Col xs={{ span: 10, offset: 1 }} md={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
-              <p id="searchErrorMessage">No se obtuvo resultados con ese nombre.</p>
-            </Col>
-            }
+          {/* Mensaje si no hubo resultados */}
+          {searchResults && searchResults.response === "error" &&
+          <Col xs={{ span: 10, offset: 1 }} md={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
+            <p id="searchErrorMessage">No se obtuvo resultados con ese nombre.</p>
+          </Col>
+          }
 
-          </Row>
+
         </Col>
       </Row>
     </Container>
