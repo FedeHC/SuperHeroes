5b0f8d21178808846df2bbeaf3ff6a24fd357f97
FedeHC <fede.hc@mail.com>
2021-08-10 18:29:53 -0300

    Agregando espacios y comentarios.

--- src/subcomponents/HeroView.js
+++ src/subcomponents/HeroView.js
@@ -9,14 +9,17 @@ function HeroView({ index,
                     deleteHeroHandler,
                     getHeroDetailsHandler }) {
   return (
-    <Col sm={12} md={6} xl={4}>
+    <Col xs={12} sm={6} lg={4} xl={3}>
       {/* CON HEROE */}
       {hero &&
         <div className={hero.biography.alignment === "good" ? "heroView goodHero" : "heroView badHero"}>
+          {/* Nombre */}
           <h1 className="heroName">{hero.name}</h1>
-          
+
+          {/* Imagen */}
           <img src={hero.image.url} className="heroImage" alt=""></img>
 
+          {/* Powerstats: */}
           <p className="powerStatsTitle">Powerstats:</p>            
           <ul className="powerStats">
             {Object.entries(hero.powerstats)
@@ -24,6 +27,7 @@ function HeroView({ index,
                     .sort()}
           </ul>
 
+          {/* Botones de opciones */}
           <div className="d-grid gap-2">
             <Button variant="outline-primary"
                     size="lg"
@@ -39,9 +43,14 @@ function HeroView({ index,
       {/* SIN HEROE*/}
       {!hero &&
         <div className="heroView">
+          {/* Título vacío */}
           <h1>[Vacío]</h1>
+
+          {/* Fondo gris (sin imagen)  */}
           <div className="emptyImage"></div>
           <br />
+          
+          {/* Botón de Agregar */}
           <div className="d-grid gap-2">
             <Button variant="outline-success"
                     size="lg"
