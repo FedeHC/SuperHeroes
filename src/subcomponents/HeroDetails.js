016f3f23ea833148e1baf0c1b6ddd5c924f44b67
FedeHC <fede.hc@mail.com>
2021-08-10 18:31:04 -0300

    Mejorando el aspecto/estilo general en componente.

--- src/subcomponents/HeroDetails.js
+++ src/subcomponents/HeroDetails.js
@@ -9,17 +9,46 @@ function HeroDetails({ hero, getMainViewHandler }) {
   return (
     <Container fluid>
       <Row>
-        <Col xs={{ span: 10, offset: 1 }} md={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
-          <div id="heroDetails"
-               className={hero.biography.alignment === "good" ? "goodHero" : "badHero"}>
-            <h1>[Detalles del Heroe]</h1>
-            <br></br>
-            <Button variant="outline-secondary"
-                        type="input"
-                        size="lg"
-                        onClick={ () => getMainViewHandler()}
-            >Volver a principal</Button>
-          </div>
+        <Col xs={{span: 10, offset: 1}}
+             className={hero.biography.alignment === "good" ? "heroDetails goodHero" : "heroDetails badHero"}>
+          
+          <Row>
+            <Col xs={12}>
+              {/* Título */}
+              <h1 id="heroDetailsTitle">[Detalles del Heroe]</h1>
+              <br />
+              {/* Nombre */}
+              <h1>Nombre: <span className="heroName">{hero.name}</span></h1>
+            </Col>
+
+            <Col xs={12} md={6}>            
+              {/* Imagen */}
+              <img src={hero.image.url} className="heroDetailsImage" alt=""></img>
+
+              {/* Powerstats */}
+              <p className="powerStatsTitle">Powerstats de {hero.name}:</p>            
+              <ul className="powerStats">
+                {Object.entries(hero.powerstats)
+                        .map( ([power, value], index) => <li key={index}>{power}: {value}</li> )
+                        .sort()}
+              </ul>
+            </Col>
+
+            {/* Detalles del Héroe/Villano */}
+            <Col xs={6}>
+              <h1 className="heroDescription">Detalles:</h1>
+            </Col>
+
+            {/* Boton Volver */}
+            <Col xs={12} className="text-center">
+              <br /><br />
+              <Button variant="outline-secondary"
+                          type="input"
+                          size="lg"
+                          onClick={ () => getMainViewHandler()}
+              >Volver a principal</Button>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
