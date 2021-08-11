77d3ef4d19691dd15f17ba91773b1e212362914e
FedeHC <fede.hc@mail.com>
2021-08-11 16:00:02 -0300

    Creando componente LoginButton.

---
+++ src/subcomponents/LoginButton.js
@@ -0,0 +1,18 @@
+// React-Bootstrap:
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+
+function LoginButton({ email, logOutHandler }) {
+  return(
+    <div id="loginButton">
+      <ButtonGroup>
+        <DropdownButton as={ButtonGroup} title={email} variant="outline-light" id="bg-nested-dropdown">
+          <Dropdown.Item eventKey="1" onClick={ () => logOutHandler()}>Cerrar sesión</Dropdown.Item>
+        </DropdownButton>
+      </ButtonGroup>
+    </div>
+  );
+}
+
+export default LoginButton;
