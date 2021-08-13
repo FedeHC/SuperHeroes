f5ed9b24a0dbb5cf8b49d5d596896d15b4d9625b
FedeHC <fede.hc@mail.com>
2021-08-13 19:32:36 -0300

    Creando componente footer.

---
+++ src/subcomponents/Footer.js
@@ -0,0 +1,27 @@
+
+
+function Footer() {
+  return (
+    <div id="footerDiv">
+      <ul>
+        <li>App creada Por FedeHC para <a href="https://github.com/FedeHC/SuperHeroes"
+                                          target="_blank"
+                                          rel="noreferrer">Alkemy Challenge Frontend</a></li>
+        <li>
+          Icons made by <a href="https://www.freepik.com"
+                          title="Freepik"
+                          target="_blank"
+                          rel="noreferrer">Freepik
+          </a> from <a href="https://www.flaticon.com/"
+                      title="Flaticon"
+                      target="_blank"
+                      rel="noreferrer">www.flaticon.com</a>
+        </li>
+
+      </ul>
+      <br /><br />
+    </div>
+  );
+}
+
+export default Footer;
