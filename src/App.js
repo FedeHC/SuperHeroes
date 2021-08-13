4b36e3e90b232f2d6706e1b401f1fc2d3999ce0c
FedeHC <fede.hc@mail.com>
2021-08-13 19:31:49 -0300

    Pasando codigo footer a subcomponente propio.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import LoginForm from "./subcomponents/LoginForm";
 import MainView from "./subcomponents/MainView";
 import HeroesSearch from "./subcomponents/HeroesSearch";
 import HeroDetails from "./subcomponents/HeroDetails";
+import Footer from "./subcomponents/Footer";
 
 // Librerías y demás:
 import axios from "axios";
@@ -256,25 +257,9 @@ function App() {
                        getMainViewHandler={getMainViewHandler} />
         </>
       }
-      <div id="footerDiv">
-        <ul>
-          <li>App creada Por FedeHC para <a href="https://github.com/FedeHC/SuperHeroes"
-                                            target="_blank"
-                                            rel="noreferrer">Alkemy Challenge Frontend</a></li>
-          <li>
-            Icons made by <a href="https://www.freepik.com"
-                             title="Freepik"
-                             target="_blank"
-                             rel="noreferrer">Freepik
-            </a> from <a href="https://www.flaticon.com/"
-                         title="Flaticon"
-                         target="_blank"
-                         rel="noreferrer">www.flaticon.com</a>
-          </li>
 
-        </ul>
-        <br /><br />
-      </div>
+      {/* En cualquier vista debe verse el footer: */}
+      <Footer />
     </>
   );
 }
