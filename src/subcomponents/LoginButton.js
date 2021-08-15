1de711368b4cb9a428349226f9f7ebf9d4445e80
FedeHC <fede.hc@mail.com>
2021-08-15 19:48:16 -0300

    Importando 'react-router' y pasando instancia a handler en Dropdown (componente bootstrap).

--- src/subcomponents/LoginButton.js
+++ src/subcomponents/LoginButton.js
@@ -3,15 +3,27 @@ import ButtonGroup from "react-bootstrap/ButtonGroup";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 
+// Librerías:
+import { useHistory } from "react-router-dom";
+
+
 function LoginButton({ email, logOutHandler }) {
+  // Para usar hook 'useHistory' de react-router:
+  let history = useHistory();
+  
   return(
     <div id="loginButton">    
       <ButtonGroup>
-        <DropdownButton as={ButtonGroup} title={email} variant="outline-light" id="bg-nested-dropdown">
+        <DropdownButton as={ButtonGroup} title={email||""} variant="outline-light" id="bg-nested-dropdown">
+          {/* Opciones falsas (deshabilitadas): */}
           <Dropdown.Item eventKey="1" disabled>Perfil</Dropdown.Item>
           <Dropdown.Item eventKey="2" disabled>Opciones</Dropdown.Item>
           <Dropdown.Divider />
-          <Dropdown.Item eventKey="3" onClick={ () => logOutHandler()}>Cerrar sesión</Dropdown.Item>
+
+          {/* Cerrar sesión: */}
+          <Dropdown.Item eventKey="3"
+                         onClick={ () => logOutHandler(history)}>Cerrar sesión</Dropdown.Item>
+
         </DropdownButton>
       </ButtonGroup>
     </div>
