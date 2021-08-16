7004332a22b4dff1b66e620d504877af7538122d
FedeHC <fede.hc@mail.com>
2021-08-16 13:01:31 -0300

    Incorporando componentes Row y Column (de Bootstrap) en Footer.

--- src/subcomponents/Footer.js
+++ src/subcomponents/Footer.js
@@ -1,41 +1,47 @@
 // React-Bootstrap:
+import Container from "react-bootstrap/Container";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import Button from "react-bootstrap/Button";
+
 
 function Footer() {
   return (
-    <div id="footerDiv">
-      <ul>
-        {/* 1° linea */}
-        <li>
-          <span>App creada Por FedeHC para</span>
-          <Button variant="link"
-                  size="sm"
-                  href="https://github.com/FedeHC/SuperHeroes"
-                  title="[GitHub] FedeHC- SuperHeroes "
-                  target="_blank"
-                  rel="noreferrer">Alkemy Challenge Frontend</Button>
-        </li>
+    <Container fluid>
+      <Row>
+        <Col md={{span: 8, offset: 2}} lg={{span: 6, offset: 3}} xxl={{span: 4, offset: 4}}>
+          <div id="footerDiv">
+            {/* 1° linea */}
+            <p>
+              <span>Creado por FedeHC para el</span>
+              <Button variant="link"
+                      size="sm"
+                      href="https://github.com/FedeHC/SuperHeroes"
+                      title="[GitHub] FedeHC- SuperHeroes "
+                      target="_blank"
+                      rel="noreferrer">Alkemy Challenge Frontend</Button>
+              <br />
 
-        {/* 2° linea */}
-        <li>
-          <span>Icons made by</span>
-          <Button variant="link"
-                  size="sm"
-                  href="https://www.freepik.com"
-                  title="Freepik"
-                  target="_blank"
-                  rel="noreferrer">Freepik</Button>
-          <span>from</span>
-          <Button variant="link"
-                  size="sm"
-                  href="https://www.flaticon.com/"
-                  title="Flaticon"
-                  target="_blank"
-                  rel="noreferrer">www.flaticon.com</Button>
-        </li>
-      </ul>
-      <br />
-    </div>
+              {/* 2° linea */}
+              <span>Icons made by</span>
+              <Button variant="link"
+                      size="sm"
+                      href="https://www.freepik.com"
+                      title="Freepik"
+                      target="_blank"
+                      rel="noreferrer">Freepik</Button>
+              <span>from</span>
+              <Button variant="link"
+                      size="sm"
+                      href="https://www.flaticon.com/"
+                      title="Flaticon"
+                      target="_blank"
+                      rel="noreferrer">www.flaticon.com</Button>
+            </p>            
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
