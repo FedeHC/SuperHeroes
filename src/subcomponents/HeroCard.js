48bd3de1ef4df22374effe00e12571aab9ed460e
FedeHC <fede.hc@mail.com>
2021-08-17 12:19:28 -0300

    Reemplanzando estructura gral. empleando componente Card (Bootstrap) en HeroCard.

--- src/subcomponents/HeroCard.js
+++ src/subcomponents/HeroCard.js
@@ -1,6 +1,8 @@
 // React-Bootstrap:
 import Col from 'react-bootstrap/Col';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
+import Card from "react-bootstrap/Card";
 import Badge from "react-bootstrap/Badge";
 
 // Librerías:
@@ -17,62 +19,83 @@ function HeroCard({ index,
   let history = useHistory();
 
   return (
-    <Col xs={12} md={6} lg={4}>
+    <Col>
       {/* CON HEROE */}
       {hero &&
-        // Div con color de fondo según el alineamiento del heroe:
-        <div className={hero.biography.alignment + "Hero heroView"}>
-
-          {/* Posición y Nombre: */}
+        // Cards con bordes de color según alineamiento del héroe:
+        <Card style={{ width: "17rem" }}
+              className={hero.biography.alignment + "Hero heroView"}>
+          
           <div className="text-center">
-            <h5><Badge bg="dark">[ {index+1} ]</Badge></h5>
-            <h1 className="heroName">{hero.name}</h1>
-          </div>
+            {/* Posición y Nombre: */}
+            <Card.Subtitle className="mb-2 text-muted">
+              <h5><Badge pill bg="dark">[ {index+1} ]</Badge></h5>
+            </Card.Subtitle>
 
+            <Card.Title>
+              <h1 className="heroName">{hero.name}</h1>
+            </Card.Title>
+          </div> 
+          
           {/* Imagen: */}
           <div className="heroImageDiv">
-            <img src={hero.image.url} className="heroImage" alt=""></img>
-          </div>
-
+            <Card.Img variant="top"
+                      src={hero.image.url}
+                      className="heroImage"
+                      alt="" />
+          </div>            
+        
           {/* Powerstats: */}
-          <p className="powerStatsTitle">Powerstats:</p>
-          <h5>{showHeroPowerstats(hero)}</h5>
-          <br />
+          <Card.Text>
+            <p className="powerStatsTitle">Powerstats:</p>
+            <p>{showHeroPowerstats(hero)}</p>
+          </Card.Text>
 
           {/* Botones de opciones: */}
-          <div className="d-grid gap-2">
+          <ButtonGroup className="d-grid gap-1">
             <Button variant="outline-primary"
-                    size="lg"
-                    onClick={ () => getDetailsViewHandler(index, history) }>Detalles</Button>
-                    
+                    size="md"
+                    onClick={ () => getDetailsViewHandler(index, history) }>Más detalles</Button>
+                  
             <Button variant="outline-danger"
-                    size="lg"
+                    size="md"
                     onClick={ () => deleteHeroHandler(index) }>Quitar del equipo</Button>
-          </div>
-        </div>
+          </ButtonGroup>
+        </Card>
       }
-      
+
       {/* SIN HEROE */}
       {!hero &&
-        <div className="heroView">
-          {/* Título vacío: */}
+        <Card style={{ width: "17rem" }}
+              className="emptyHero heroView">
+          
           <div className="text-center">
-            <h5><Badge bg="dark">[ {index+1} ]</Badge></h5>
-            <h1 className="heroName">[ Vacante ]</h1>
-          </div>
+            {/* Posición y Nombre: */}
+            <Card.Subtitle className="mb-2 text-muted">
+              <h5><Badge pill bg="secondary">[ {index+1} ]</Badge></h5>
+            </Card.Subtitle>
 
-          {/* Fondo gris representando "sin imagen": */}
-          <div className="emptyImage"></div>
-          <br />
+            <Card.Title>
+              <h1 className="heroName">[ Vacante ]</h1>
+            </Card.Title>
+          </div> 
+          
+          {/* Imagen: */}
+          <div className="heroDetailsImage">
+            <Card.Img variant="top"
+                      className="emptyImage"
+                      alt="" />
+          </div>
           
           {/* Botón de Agregar: */}
-          <div className="d-grid gap-2">
-            <Button variant="outline-success"
-                    size="lg"
-                    type="input"
-                    onClick={ () => getSearchView(index, history)}>Agregar al equipo</Button>
-          </div>
-        </div>
+            <ButtonGroup className="d-grid gap-1">
+              <Button variant="outline-success"
+                      size="md"
+                      type="input"
+                      onClick={ () => getSearchView(index, history)}>Agregar al equipo</Button>
+            </ButtonGroup>
+
+        </Card>
       }
     </Col>
   );
@@ -84,10 +107,9 @@ function showHeroPowerstats(hero) {
   return Object.entries(hero.powerstats)
                .map( ([power, value], index) => (
                     <span key={index}>
-                      <Badge bg="dark">{power}<span> </span>
+                      <Badge pill bg="dark">{power}<span> </span>
                         <Badge bg="secondary">{value === "null" ? "---" : value}</Badge> {/* Algunos valores pueden llegar 'null'. */}
                       </Badge>
-                      <br />
                     </span>
                ));
 }
